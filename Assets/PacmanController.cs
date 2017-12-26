c7ffa808e449a107fca36d0933bf07b471ab4666
Step <stephanwells@hotmail.com>
2017-12-26 19:35:54 +0100

    >Fixed a number of issues with sprite animation. >Implemented simple Pacman movement.

---
+++ Assets/PacmanController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanController : MonoBehaviour
+{
+    public float playerSpeed;
+
+	void Update ()
+    {
+		if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(Vector2.right * playerSpeed);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(Vector2.left * playerSpeed);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector2.up * playerSpeed);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector2.down * playerSpeed);
+        }
+    }
+}
