5fe3150726ef84d23525d7151fb2f2e040b43e4b
Step <stephanwells@hotmail.com>
2017-12-29 21:41:51 +0100

    >Made Pac-Man controllable by the character with proper animation state transitions. >Tweaked some animation and transition parameters.

--- Assets/PacmanController.cs
+++ Assets/PacmanController.cs
@@ -5,24 +5,50 @@ using UnityEngine;
 public class PacmanController : MonoBehaviour
 {
     public float playerSpeed;
+    Animator animator;
 
-	void Update ()
+    void Start()
+    {
+        animator = this.GetComponent<Animator>();
+    }
+
+    void Update ()
     {
 		if (Input.GetKey(KeyCode.RightArrow))
         {
             transform.Translate(Vector2.right * playerSpeed);
+
+            animator.SetBool("Right", true);
+            animator.SetBool("Left", false);
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
         }
         else if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.Translate(Vector2.left * playerSpeed);
+
+            animator.SetBool("Right", false);
+            animator.SetBool("Left", true);
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", false);
         }
         else if (Input.GetKey(KeyCode.UpArrow))
         {
             transform.Translate(Vector2.up * playerSpeed);
+
+            animator.SetBool("Right", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Up", true);
+            animator.SetBool("Down", false);
         }
         else if (Input.GetKey(KeyCode.DownArrow))
         {
             transform.Translate(Vector2.down * playerSpeed);
+
+            animator.SetBool("Right", false);
+            animator.SetBool("Left", false);
+            animator.SetBool("Up", false);
+            animator.SetBool("Down", true);
         }
     }
 }
