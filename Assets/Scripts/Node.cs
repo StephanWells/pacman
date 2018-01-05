4bd5d33b7e2075016bed71b0765bc42133e5c16e
Step <stephanwells@hotmail.com>
2018-01-05 01:22:00 +0100

    >Reworked positions of game objects due to a major inconsistency in local vs global coordinates. >Implemented infrastructure for node pathing. >Added and edited some scripts for node pathing (still in an intermediate state). >Defined script execution order in proejct settings.

---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Node[] neighbouringNodes; // All the neighbouring nodes that Pacman can go to.
+    public Vector2[] validDirections; // All the directions Pacman can choose from at the current node.
+
+	void Start ()
+    {
+        validDirections = new Vector2[neighbouringNodes.Length];
+
+        // Go through each neighbouring node, find the direction Pacman can take to get to it, and add it to validDirections.
+        for (int i = 0; i < neighbouringNodes.Length; i++)
+        {
+            validDirections[i] = (neighbouringNodes[i].transform.localPosition - this.transform.localPosition).normalized;
+        }
+	}
+}
---
+++ Assets/Scripts/PacmanController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PacmanController : MonoBehaviour
+{
+    enum directions { RIGHT, UP, DOWN, LEFT, STATIONARY };
+
+    public float playerSpeed;
+    private Vector2 playerDirection;
+    private Animator animator;
+    private directions directionState;
+    public Node currentNode;
+    public Node nextNode;
+
+    void Start()
+    {
+        directionState = directions.STATIONARY;
+        animator = this.GetComponent<Animator>();
+        nextNode = null;
+    }
+
+    void Update()
+    {
+        CheckInput();
+        //CheckNodes();
+        ChangeDirection();
+        Move();
+    }
+
+    //void CheckNodes()
+    //{
+    //    if (this.transform.localPosition.Equals(nextNode.transform.localPosition))
+    //    {
+    //        currentNode = nextNode;
+    //    }
+    //}
+
+    void ChangeDirection()
+    {
+        switch (directionState)
+        {
+            case directions.RIGHT :
+                playerDirection = Vector2.right;
+                SetAnimatorState("Right");
+            break;
+
+            case directions.UP:
+                playerDirection = Vector2.up;
+                SetAnimatorState("Up");
+            break;
+
+            case directions.LEFT:
+                playerDirection = Vector2.left;
+                SetAnimatorState("Left");
+            break;
+
+            case directions.DOWN:
+                playerDirection = Vector2.down;
+                SetAnimatorState("Down");
+            break;
+
+            case directions.STATIONARY:
+                playerDirection = Vector2.zero;
+            break;
+        }
+    }
+
+    void CheckInput()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            directionState = directions.RIGHT;
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            directionState = directions.UP;
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            directionState = directions.LEFT;
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            directionState = directions.DOWN;
+        }
+    }
+
+    void Move()
+    {
+        transform.localPosition += (Vector3)(playerDirection * playerSpeed) * Time.deltaTime;
+    }
+
+    void SetAnimatorState(string state)
+    {
+        animator.SetBool("Right", false);
+        animator.SetBool("Left", false);
+        animator.SetBool("Up", false);
+        animator.SetBool("Down", false);
+        animator.SetBool(state, true);
+    }
+}
